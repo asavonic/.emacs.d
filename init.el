fd72cdad590cb8cb6eb9cc3ea3475107cabed2bf
Andrew Savonichev <andrew.savonichev@gmail.com>
2017-07-16 18:03:59 +0300

    Add ivy, swiper and counsel

--- init.el
+++ init.el
@@ -7,11 +7,10 @@
 (load (concat my/config-dir "/appearance.el"))
 
 
+(setq indent-tabs-mode nil)
+
 
 (my/add-package "utils/with-editor")
-(use-package with-editor)
-
-
 (my/add-package "dev/magit/lisp")
 (use-package magit
   :bind
@@ -24,3 +23,52 @@
 (my/add-package "utils/ace-jump")
 (use-package ace-jump-mode
   :bind (("M-z" . ace-jump-mode)))
+
+
+;; swiper with ivy-mode - lightweight helm alternative
+(my/add-package "utils/swiper")
+(use-package ivy
+  :bind (("C-c C-r" . ivy-resume))
+  :config
+  (setq ivy-count-format "(%d/%d) ")
+  (setq ivy-height 20))
+
+(use-package swiper
+  :bind
+  ("C-s" . swiper))
+
+(use-package counsel
+  :config
+  (defun my/counsel-pt (&optional initial-input initial-dir)
+    (interactive)
+    (when (use-region-p)
+      (setq initial-input (buffer-substring (mark) (point))))
+
+    (unless initial-dir
+      (setq initial-dir (ido-read-directory-name "Search in: "
+                                                 default-directory)))
+    (let ((counsel-ag-base-command counsel-pt-base-command))
+      (counsel-ag initial-input initial-dir)))
+
+  (defun my/counsel-pt-dwim-directory-hint () nil)
+
+  (defun my/counsel-pt-dwim ()
+    (interactive)
+    (my/counsel-pt nil (my/counsel-pt-dwim-directory-hint)))
+
+  (defun my/counsel-yank-pop ()
+    (interactive)
+    "If a previous command was not `yank', `yank-pop' will"
+    (if (and (not (eq last-command 'yank))
+             (not (active-minibuffer-window)))
+        (counsel-yank-pop)
+      (yank-pop)))
+
+  :bind
+  ("M-x"     . counsel-M-x)
+  ("C-x l"   . counsel-locate)
+  ("C-x r b" . counsel-bookmark)
+  ("C-c s s" . counsel-dwim)
+  ("C-c s S" . counsel-pt)
+  ("C-c i"   . counsel-imenu)
+  ("M-y"     . my/counsel-yank-pop))
