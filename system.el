ecce50de01b760287f43d04732972ca2d264c54f
Andrew Savonichev <andrew.savonichev@gmail.com>
2017-07-24 02:04:02 +0300

    Add clipboard handler

--- system.el
+++ system.el
@@ -9,3 +9,18 @@
 ;; Without internet connection, this may take a while before timeout.
 (defvar tramp-ssh-controlmaster-options
       "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
+
+
+(defcustom my/clipboard-handler-default-buffer "*clipboard*"
+  "Default buffer to use for `my/clipboard-handler'")
+
+(defun my/clipboard-handler (&optional buffer-or-name)
+  (unless buffer-or-name
+    (setq buffer-or-name my/clipboard-handler-default-buffer))
+
+  (switch-to-buffer buffer-or-name)
+
+  (end-of-buffer)
+  (newline)
+  (insert (shell-command-to-string "xclip -o"))
+  t)
