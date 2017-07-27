6a112cd3a3219fa4f05bd0014129c91084675da0
Andrew Savonichev <andrew.savonichev@gmail.com>
2017-07-27 13:12:15 +0300

    Add functions to work with config submodules

--- utils.el
+++ utils.el
@@ -6,7 +6,10 @@
 
 (defun my/add-package (p)
   (let ((dir (my/package-dir p)))
-    (unless (file-exists-p dir)
+    (unless (or (file-exists-p dir)
+                (-some (lambda (ignore)
+                         (string-match-p ignore dir))
+                       my/submodule-ignore-list))
       (when (yes-or-no-p
 	     (format "Package %s does not exist. Clone instead? " p))
 	(my/clone-package p)))
@@ -31,6 +34,31 @@
 	(progn
 	  (display-buffer buffer)
 	  (error "Cloning %s ... failed" p))))))
+
+
+(defcustom my/submodule-ignore-list '()
+  "List of submodule which should not be updated or synced")
+
+(defun my/submodule-sync ()
+  (interactive)
+  (let ((ignored 0))
+    (with-temp-buffer
+      (let ((buffer (current-buffer)))
+	(call-process "git" nil (current-buffer) nil "submodule" "status")
+	(beginning-of-buffer)
+	(while (not (eobp))
+	  (when (looking-at "^-[[:xdigit:]]+ \\(.+\\)$")
+	    (let ((submodule (match-string 1)))
+	      (if (member submodule my/submodule-ignore-list)
+		  (setq ignored (+ ignored 1))
+		(progn
+		  (message "Updating submodule: %s" submodule)
+		  (with-current-buffer "*submodule sync*"
+		    (call-process "git" nil (current-buffer) nil
+				  "submodule" "update" "--init" "--"
+				  submodule))))))
+	  (forward-line 1))))
+    (message "Sync done (ignored submodules: %d)" ignored)))
 
 ;; Load essential packages upfront
 
