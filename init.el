635833a626157cdd40422c4bcfbc0d5dab4e4f2c
Andrew Savonichev <andrew.savonichev@gmail.com>
2018-10-10 12:58:39 +0300

    New package: magit

--- init.el
+++ init.el
@@ -4,3 +4,14 @@
 
 (my/add-package "use-package")
 (require 'use-package)
+
+(my/add-package "dash")
+(my/add-package "treepy")
+(my/add-package "with-editor")
+(my/add-package "ghub")
+(my/add-package "graphql")
+(my/add-package "magit-popup")
+(my/add-package "magit")
+(my/add-package "magit/lisp")
+(use-package magit
+  :bind ("C-c g s" . magit-status))
